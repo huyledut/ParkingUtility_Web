40288f7416fbe316ec3556ff665eb1dfe0866919
huyledut <huyledut@gmail.com>
2022-11-27 16:31:59 +0700

    [DUT-3] [FE] Login UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@ module.exports = {
   content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   darkMode: 'class',
   theme: {
+    color: {
+      'tahiti': {
+        100: '#cffafe',
+        200: '#a5f3fc',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#0e7490',
+        800: '#155e75',
+        900: '#164e63',
+        1000: '#1f2938'
+      },
+    },
     fontFamily: {
       display: ['Oswald', 'sans-serif'],
       body: ['Poppins', 'sans-serif'],
